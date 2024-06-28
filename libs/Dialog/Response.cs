f6f8d6c8e0e316fb97ffc8f0543a08568d577bd2
seoyangjadu <plum.blossom.haejin@gmail.com>
2024-06-28 10:35:23 +0200

    added comments to Response

--- libs/Dialog/Response.cs
+++ libs/Dialog/Response.cs
@@ -1,9 +1,14 @@
 namespace libs;
 
+// class representing a response in the dialog
 public class Response {
+    // the textual content of the response
     public string text;
+
+    // the dialog node it goes to after choosing the response
     public DialogNode? nextNode;
 
+    // constructor initializing the response with text and next node 
     public Response(string text, DialogNode? nextNode)
     {
         this.text = text;
