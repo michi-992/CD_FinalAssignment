baa98c7534e10fa218c8086fff2993017e4d3166
seoyangjadu <plum.blossom.haejin@gmail.com>
2024-06-28 10:35:13 +0200

    added comments to DialogNode

--- libs/Dialog/DialogNode.cs
+++ libs/Dialog/DialogNode.cs
@@ -3,15 +3,20 @@ using System.Net;
 
 namespace libs;
 
+// class representing a node in the dialog tree
 public class DialogNode {
 
+    // textual content of the dialog node
     public string text;
+
+    // list of possible responses from the node
     public List<Response> responses = new List<Response>();
 
     public DialogNode (string text) {
-        this.text = text;
+        this.text = text; // contructor initializing the node with text
     }
 
+    // method to add a response to the list of responses of the node
     public void AddResponse(string responseText, DialogNode? nextNode)
     {
         this.responses.Add(new Response(responseText, nextNode));
